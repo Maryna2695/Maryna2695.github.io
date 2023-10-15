3696e8c04cf116d8772c95ac0a44cc08cac36c2d
hmaryna <slipchenkomarina26@gmail.com>
2023-10-16 01:58:16 +0300

    index.html

---
+++ main.js
@@ -0,0 +1,4 @@
+const changeBackgroundButton= document.querySelector("#change-background");
+changeBackgroundButton.addEventListener("click", function() {
+  document.body.style.backgroundColor = "rgba(86,150,87,0.62)";
+});
